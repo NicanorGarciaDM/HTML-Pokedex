9d1378b6ae8cf3f5f1424e43beba4c818b2aba3a
NicanorGarciaDM <nicanorgarcia299@gmail.com>
2024-06-23 11:49:50 -0300

    Nombres corregidos

--- scripts.js
+++ scripts.js
@@ -26,6 +26,73 @@ const typeColors = {
     fairy: '#D685AD'
 };
 
+// Nombres personalizados para ciertos Pokémon
+const customNames = {
+    29: 'Nidoran F',
+    32: 'Nidoran M',
+    122: 'Mr. Mime',
+    386: 'Deoxys',
+    413: 'Wormadam',
+    439: 'Mime Jr.',
+    474: 'Porygon-Z',
+    487: 'Giratina',
+    492: 'Shaymin',
+    550: 'Basculin',
+    555: 'Darmanitan',
+    641: 'Tornadus',
+    642: 'Thundurus',
+    645: 'Landorus',
+    647: 'Keldeo',
+    648: 'Meloetta',
+    678: 'Meowstic',
+    681: 'Aegislash',
+    710: 'Pumpkaboo',
+    711: 'Gourgeist',
+    718: 'Zygarde',
+    741: 'Oricorio',
+    745: 'Lycanroc',
+    746: 'Wishiwashi',
+    772: 'Type: Null',
+    774: 'Minior',
+    778: 'Mimikyu',
+    785: 'Tapu Koko',
+    786: 'Tapu Lele',
+    787: 'Tapu Bulu',
+    788: 'Tapu Fini',
+    849: 'Toxtricity',
+    866: 'Mr. Rime',
+    875: 'Eiscue',
+    876: 'Indeedee',
+    877: 'Morpeko',
+    892: 'Urshifu',
+    902: 'Basculegion',
+    905: 'Enamorus',
+    984: 'Great Tusk',
+    985: 'Scream Tail',
+    986: 'Brute Bonnet',
+    987: 'Flutter Mane',
+    988: 'Slither Wing',
+    989: 'Sandy Shocks',
+    990: 'Iron Treads',
+    991: 'Iron Bundle',
+    992: 'Iron Hands',
+    993: 'Iron Jugulis',
+    994: 'Iron Moth',
+    995: 'Iron Thorns',
+    1001: 'Wo-Chien',
+    1002: 'Chien-Pao',
+    1003: 'Ting-Lu',
+    1004: 'Chi-Yu',
+    1005: 'Roaring Moon',
+    1006: 'Iron Valiant',
+    1009: 'Walking Wake',
+    1010: 'Iron Leaves',
+    1020: 'Gouging Fire',
+    1021: 'Raging Bolt',
+    1022: 'Iron Boulder',
+    1023: 'Iron Crown',
+};
+
 const fetchPokemonData = async () => {
     for (let i = 1; i <= 1025; i++) { 
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
@@ -58,7 +125,10 @@ const createPokemonCard = (pokemon) => {
     
     const name = document.createElement('span');
     name.classList.add('nombre-pokemon');
-    name.textContent = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    
+    // Usar el nombre personalizado si existe, de lo contrario usar el nombre original
+    const pokemonName = customNames[pokemon.id] || (pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1));
+    name.textContent = pokemonName;
     
     card.appendChild(number);
     card.appendChild(image);
